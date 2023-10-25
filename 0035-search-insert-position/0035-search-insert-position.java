579d209798ae06327c9586fe1505f7818376769e
Murilo Rezende <52568885+Procedeus@users.noreply.github.com>
2023-10-25 15:18:27 -0300

    Time: 0 ms (100%), Space: 43 MB (67.65%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,16 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int index = 0;
-        boolean first = true;
-        for(int i = 0; i < nums.length && first; i++){
-            if(nums[i] < target && first){
-                index = i + 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > target){
+                return i;
             }
             else if(nums[i] == target){
-                index = i;
-                first = false;
+                return i;
+            }
+            else if(i == nums.length - 1){
+                return i + 1;
             }
         }
-        return index;
+        return 0;
     }
 }
